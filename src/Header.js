93e6b3837c78ff3bad147e0e20fbe8a590211ea2
huijjj <hui0213@postech.ac.kr>
2021-08-09 18:16:43 +0900

    fix: improve style

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import logo from './img/logo.png';
+import './css/Header.css';
 
 function Header() {
   return (
     <div style={{"height":"10%"}}>
-        <Link to="/" className='header__logo'><img src={logo} alt='logo' /></Link>
+        <Link to="/" className='headerLogo'>Hui.GG</Link>
         <hr style={{"margin":"0"}} />
     </div>
-  )
+  );
 }
 
 export default Header;
